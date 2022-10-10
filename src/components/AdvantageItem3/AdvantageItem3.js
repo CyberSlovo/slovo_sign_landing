4b753a2b25049eac694c5d872490169d62f92353
RakhimyanTim <rahimtoy@gmail.com>
2022-10-10 22:47:15 +0500

    Добавил тултип

--- src/components/AdvantageItem3/AdvantageItem3.js
+++ src/components/AdvantageItem3/AdvantageItem3.js
@@ -3,6 +3,7 @@ import Container from "../Container/Container";
 import "./AdvantageItem3.css";
 import IcQuestionCircle1 from "../../assets/images/ic-question-circle-1@2x.svg";
 import IcSberTime from "../../assets/images/icon-sber-time@2x.svg";
+import ReactTooltip from "react-tooltip";
 
 function AdvantageItem3(props) {
   const { containerProps } = props;
@@ -10,7 +11,19 @@ function AdvantageItem3(props) {
   return (
     <div className="advantage-item-4">
       <Container title={containerProps.title} subtitle={containerProps.subtitle} className={containerProps.className} src={IcSberTime} />
-      <img className="ic-question-circle" src={IcQuestionCircle1} alt="ic question-circle" />
+      <img data-tip data-for="registerTip" className="ic-question-circle" src={IcQuestionCircle1} alt="ic question-circle" />
+      <ReactTooltip 
+        id="registerTip" 
+        place="bottom" 
+        afterHide={true} 
+        backgroundColor="white"
+        multiline={true}
+        textColor="rgba(38, 38, 38, 0.7)"
+        border="1px"
+        borderColor="rgba(38, 38, 38, 0.08)"
+      >
+        УНЭП — это усиленная неквалифицированная подпись
+      </ReactTooltip>
     </div>
   );
 }
