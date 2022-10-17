efbdacd729081d9fc05ffd7186948acf06e178e5
RakhimyanTim <rahimtoy@gmail.com>
2022-10-17 17:33:03 +0500

    Добавил ховеры

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,25 @@
 import React from "react";
 import "./Button.css";
+import { useState } from 'react';
 
 function Button(props) {
   const { onclick, children } = props;
 
+  const [isHover, setIsHover] = useState(false);
+
+  const handleMouseEnter = () => {
+     setIsHover(true);
+  };
+  const handleMouseLeave = () => {
+     setIsHover(false);
+  };
+
   return (
-    <div onClick={onclick} className="button">
+    <div 
+    onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave}
+    onClick={onclick} 
+    className="button">
       <div className="frame-270988836">
         <div className="button-1">{children}</div>
       </div>
--- src/components/Button2/Button2.js
+++ src/components/Button2/Button2.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { useState } from 'react';
 import "./Button2.css";
 
 function Button2(props) {
   const { onclick } = props;
+  
+  const [isHover, setIsHover] = useState(false);
+
+  const handleMouseEnter = () => {
+     setIsHover(true);
+  };
+  const handleMouseLeave = () => {
+     setIsHover(false);
+  };
   return (
-    <div onClick={onclick} className="button-2">
+    <div 
+    onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave}
+    onClick={onclick} 
+    className="button-2">
       <div className="frame-270988836-1">
         <div className="button-3 sbsanstext-regular-normal-white-19px">Подробнее</div>
       </div>
