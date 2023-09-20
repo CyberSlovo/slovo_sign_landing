b4a8d9561673caff9ca26fb07181d67cd4576f6c
Dmitry <deorditse.d@yandex.com>
2023-09-20 17:18:57 +0300

    version-1.1.50

--- src/components/MaleLaptop/index.jsx
+++ src/components/MaleLaptop/index.jsx
@@ -2,19 +2,14 @@ import React from "react";
 import "./MaleLaptop.css";
 
 function MaleLaptop(props) {
-  const { className, maleLaptop08Props, isMobile } = props;
+  const { className, isMobile } = props;
 
   return (
     <div
       className={
         isMobile ? className || "" : `male-laptop-1 ${className || ""}`
       }
-    >
-      {/* <MaleLaptop08
-        src={maleLaptop08Props.src}
-        className={maleLaptop08Props.className}
-      /> */}
-    </div>
+    ></div>
   );
 }
 
